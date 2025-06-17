a24b11b319aba1cce257c8bc48b03cabdbae0379
ilankumaran <kumaranilan@yahoo.com>
2025-06-17 10:35:24 +0100

    @EnableSpringDataWebSupport to stable JSON support

--- src/main/java/com/ilan/H2Application.java
+++ src/main/java/com/ilan/H2Application.java
@@ -6,9 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @Slf4j
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class H2Application {
 
     public static void main(String[] args) {
